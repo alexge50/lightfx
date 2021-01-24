ab1c3bbb9ea56f1e329f1f129860d4f5494f9166
alexge50 <alexge50.x@gmail.com>
2021-01-24 16:48:19 +0000

    added main loop

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+
+from core.context import Context
+import core.effects
+from config import *
+
+
+async def main_loop():
+    context = Context(SINKS)
+
+    for effect_file in EFFECTS:
+        await context.add_effects(core.effects.load_effects(effect_file))
+
+    await context.set_effect(DEFAULT_EFFECT)
+
+    while True:
+        await context.frame()
+
+        delta_time = await context.get_delta_time()
+        effect_description = await context.get_effect_description()
+        result = None
+
+        if core.effects.is_effect_function(effect_description[0]):
+            result = effect_description[0](delta_time, effect_description[1])
+        elif effect_description[0] is not None:
+            result = effect_description[0].run(delta_time, effect_description[1])
+
+        for sink in context.sinks:
+            await sink.sink(result)
+
+asyncio.run(main_loop())
