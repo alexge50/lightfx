d9d706d5d6b391f14a240e548fdb1e402a7d9a64
alexge50 <alexge50.x@gmail.com>
2021-01-26 04:44:56 +0000

    solved conversion error in timer.py

--- core/timer.py
+++ core/timer.py
@@ -4,10 +4,10 @@ import time
 class Timer:
     def __init__(self, sleep=16):
         self.delta_time = 0
-        self._previous_time = time.time_ns() / 1000
-        self._time = time.time_ns() / 1000
+        self._previous_time = time.time_ns() / 1000000
+        self._time = time.time_ns() / 1000000
 
     def frame(self):
-        self._time = time.time_ns() / 1000
+        self._time = time.time_ns() / 1000000
         self.delta_time = self._time - self._previous_time
         self._previous_time = self._time
